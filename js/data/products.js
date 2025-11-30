0fc450fb70af38595df75da3c6b84bb2e323d282
jalaldevs <voiceofjalal@gmail.com>
2025-11-30 09:52:44 +0100

    First New Commit

---
+++ js/data/products.js
@@ -0,0 +1,21 @@
+const featured = [
+    {
+    image : "" 
+    name: "Travel Backpack",
+    price: 7999,
+    rating: {
+        stars: 4.6,
+        reviews: 1024
+    },
+    quantity: 1
+    },
+    {
+    name : "Cozy Fleece Hoodie",
+    price : 4999,
+    rating: {
+        stars: 4.3,
+        reviews: 387
+    },
+    quantity: 1
+    }
+];
--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 // Header
 
+const searchTextWrapper = document.getElementById('search-text-wrapper');
 const searchInput = document.getElementById('search-bar');
-const headerSearch = document.querySelector('.nav_li > .nav_link:first-child');
+const headerSearch = document.querySelector('.nav_link:first-child');
 const searchText = document.querySelector('.search-text');
+const searchIcon = document.getElementById('search-icon');
+
+searchTextWrapper.addEventListener('click', () => {
+    searchInput.classList.toggle('search-input-toggled');
+    searchText.classList.toggle('search-text-toggled');
+    searchIcon.classList.toggle('search-icon-toggled')
+})
+
 
 // Mobile Header
 const mobileSearchIcon = document.querySelector('.js-mobile-search-icon');
@@ -31,3 +40,93 @@ mediaQuery.addEventListener("change", () => {
         }
     }
 });
+
+// FEATURED PRODUCTS
+
+// FEATURED PRODUCTS
+const featuredProducts = document.getElementById('js-featured-products');
+
+let featuredProductsHTML = `
+<article class="featured">
+    <div class="featured_image">
+        <img src="./images/backpack (2).jpg" alt="backpack image">
+    </div>
+    <div class="featured_info">
+        <div class="featured-title">
+            <h2>Travel Backpack</h2>
+        </div>
+        <div class="featured-price">
+            <p>$79.99</p>
+        </div>
+        <div class="featured-stars">
+            <p><i class="fas fa-star"></i> 4.6/5 — 1,024 reviews</p>
+        </div>
+        <div class="featured-buy">
+            <button>Buy Now <span><i class="fas fa-credit-card"></i></span></button>
+            <button>Add To Cart <span><i class="fas fa-shopping-cart"></i></span></button>
+        </div>
+    </div>
+</article>
+<article class="featured">
+    <div class="featured_image">
+        <img src="./images/men-cozy-fleece-zip-up-hoodie-red.jpg" alt="men hoodie image">
+    </div>
+    <div class="featured_info">
+        <div class="featured-title">
+            <h2>Cozy Fleece Hoodie</h2>
+        </div>
+        <div class="featured-price">
+            <p>$49.99</p>
+        </div>
+        <div class="featured-stars">
+            <p><i class="fas fa-star"></i> 4.3/5 — 387 reviews</p>
+        </div>
+        <div class="featured-buy">
+            <button>Buy Now <span><i class="fas fa-credit-card"></i></span></button>
+            <button>Add To Cart <span><i class="fas fa-shopping-cart"></i></span></button>
+        </div>
+    </div>
+</article>
+<article class="featured js-featured-display-none">
+    <div class="featured_image">
+        <img src="./images/electric-glass-and-steel-hot-water-kettle.webp" alt="kettle image">
+    </div>
+    <div class="featured_info">
+        <div class="featured-title">
+            <h2>Smart Electric Kettle</h2>
+        </div>
+        <div class="featured-price">
+            <p>$39.99</p>
+        </div>
+        <div class="featured-stars">
+            <p><i class="fas fa-star"></i> 4.1/5 — 290 reviews</p>
+        </div>
+        <div class="featured-buy">
+            <button>Buy Now <span><i class="fas fa-credit-card"></i></span></button>
+            <button>Add To Cart <span><i class="fas fa-shopping-cart"></i></span></button>
+        </div>
+    </div>
+</article>
+<article class="featured">
+    <div class="featured_image">
+        <img src="./images/plain-hooded-fleece-sweatshirt-yellow (1).jpg" alt="yellow hoodie image">
+    </div>
+    <div class="featured_info">
+        <div class="featured-title">
+            <h2>Everyday Hoodie</h2>
+        </div>
+        <div class="featured-price">
+            <p>$59.99</p>
+        </div>
+        <div class="featured-stars">
+            <p><i class="fas fa-star"></i> 4.8/5 — 2,504 reviews</p>
+        </div>
+        <div class="featured-buy">
+            <button>Buy Now <span><i class="fas fa-credit-card"></i></span></button>
+            <button>Add To Cart <span><i class="fas fa-shopping-cart"></i></span></button>
+        </div>
+    </div>
+</article>
+`;
+
+featuredProducts.innerHTML += featuredProductsHTML;
